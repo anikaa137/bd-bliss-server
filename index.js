6de1261e8e3c0aa117662c984795105fb83ae9ed
anikaa137 <anikaa137@gmail.com>
2021-05-31 16:44:00 +0600

    single id

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const app = express()
-const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
-const  bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
-console.log(process.env.DB_USER,process.env.DB_PASS,process.env.DB_NAME )
-
-
-const port = process.env.PORT || 5000;
+// console.log(process.env.DB_USER,process.env.DB_PASS,process.env.DB_NAME )
+const port = process.env.PORT || 5050;
 
 app.use(cors());
-app.use(express.json())
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -23,13 +22,36 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   console.log('err payce', err)
   const glassCollection = client.db("bdbliss").collection("glass");
-   console.log('data connected')
+  console.log('data connected')
 
-});
+  app.post('/addProduct', (req, res) => {
+    // console.log('sdfs',req)
+    const newProduct = req.body;
+    console.log('adding pro', newProduct)
+    glassCollection.insertOne(newProduct)
+      .then(result => {
+        console.log('inserted count', result.insertedCount)
+        res.send(result.insertedCount > 0)
+      })
+  })
+
+  app.get('/product', (req, res) => {
+    glassCollection.find()
+      .toArray((err, items) => {
+        res.send(items)
+        // console.log('from database', items)
+      })
+  })
+
+  app.get('/product/:id', (req, res) => {
+    glassCollection.find({ _id: ObjectId(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents[0])
+        console.log(err, documents)
+      })
+  });
 
 
+})
 
-
-
-
-app.listen(port)
+app.listen(port);
